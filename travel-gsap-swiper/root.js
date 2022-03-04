172a63f943b5b7b9b65ecfaf33dd5d7fb3449922
mr-teslaa <mr.teslaa@protonmail.com>
2022-03-04 09:46:31 +0600

    fixed travel landing page

--- travel-gsap-swiper/root.js
+++ travel-gsap-swiper/root.js
@@ -1,114 +1,70 @@
-/* ================== SHOW MENU ================== */
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId);
-    
-    /* validate the veriable exist */
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            // we added the 'show-menu' class in the div tag
-            nav.classList.toggle('show-menu');
-            console.log('done');
-        })
-    } else {
-        console.log('problem');
-    }
-}
-
-showMenu('nav-toggle', 'nav-id');
-
-/* ================== SWIPER JS ================== */
+    toggle && nav
+        ? toggle.addEventListener("click", () => {
+              nav.classList.toggle("show-menu"), console.log("done");
+          })
+        : console.log("problem");
+};
+showMenu("nav-toggle", "nav-id");
 let galleryThumbs = new Swiper(".gallery-thumbs", {
-    spaceBetween: 0,
-    slidesPerView: 0,
-  });
-
-let galleryTop = new Swiper(".gallery-top", {
-    effect: 'fade',
-    loop: true,
-
-    thumbs: {
-      swiper: galleryThumbs
-    }
-});
-
-
-/* ================== POPUP ================== */
-const btnOpenVideo = document.querySelectorAll('.islands__video-content'),
-    islandsPopup = document.querySelector('#popup'),
-    btnCloseVideo = document.querySelector('.islands__popup-close');
-
+        spaceBetween: 0,
+        slidesPerView: 0,
+    }),
+    galleryTop = new Swiper(".gallery-top", {
+        effect: "fade",
+        loop: !0,
+        thumbs: { swiper: galleryThumbs },
+    });
+const btnOpenVideo = document.querySelectorAll(".islands__video-content"),
+    islandsPopup = document.querySelector("#popup"),
+    btnCloseVideo = document.querySelector(".islands__popup-close");
 function poPup() {
-    islandsPopup.classList.add('show-popup');
-    console.log(btnOpenVideo);
-    console.log(islandsPopup);
+    islandsPopup.classList.add("show-popup"),
+        console.log(btnOpenVideo),
+        console.log(islandsPopup);
 }
-
-btnOpenVideo.forEach(b => {
-    b.addEventListener('click', poPup);
-});
-
-btnCloseVideo.addEventListener('click', () => {
-    islandsPopup.classList.remove('show-popup');
-});
-
-/* ================== GSAP ANIMATION ================== */
-const controlImg = document.querySelectorAll('.controls__img');
-
+btnOpenVideo.forEach((b) => {
+    b.addEventListener("click", poPup);
+}),
+    btnCloseVideo.addEventListener("click", () => {
+        islandsPopup.classList.remove("show-popup");
+    });
+const controlImg = document.querySelectorAll(".controls__img");
 function ScrollAnimation() {
-    gsap.from(
-        '.islands__subtitle',
-        {
+    gsap.from(".islands__subtitle", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2,
+        y: -20,
+    }),
+        gsap.from(".islands__title", {
             opacity: 0,
-            duration: .2,
-            delay: .2,
+            duration: 0.3,
+            delay: 0.3,
             y: -20,
-        }
-    );
-    
-    gsap.from(
-        '.islands__title',
-        {
+        }),
+        gsap.from(".islands__description", {
             opacity: 0,
-            duration: .3,
-            delay: .3,
+            duration: 0.4,
+            delay: 0.4,
             y: -20,
-        }
-    );
-    
-    gsap.from(
-        '.islands__description',
-        {
+        }),
+        gsap.from(".islands__button", {
             opacity: 0,
-            duration: .4,
-            delay: .4,
+            duration: 0.5,
+            delay: 0.5,
             y: -20,
-        }
-    );
-    
-    gsap.from(
-        '.islands__button',
-        {
+        }),
+        gsap.from(".islands__video-content", {
             opacity: 0,
-            duration: .5,
-            delay: .5,
+            duration: 0.6,
+            delay: 0.6,
             y: -20,
-        }
-    );
-    
-    gsap.from(
-        '.islands__video-content',
-        {
-            opacity: 0,
-            duration: .6,
-            delay: .6,
-            y: -20,
-        }
-    );
-
-    islandsPopup.classList.remove('show-popup')
+        }),
+        islandsPopup.classList.remove("show-popup");
 }
-
-controlImg.forEach(c => {
-    c.addEventListener('click', ScrollAnimation);
+controlImg.forEach((c) => {
+    c.addEventListener("click", ScrollAnimation);
 });
